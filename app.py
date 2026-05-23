2cc69163e45773d64381177ca1594be49b33e608
augustoathiago <augusto.a.thiago@gmail.com>
2026-05-23 10:37:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -484,7 +484,7 @@ Vp = finite_sum(V1, V2)
 # =========================================================
 st.markdown("## Imagem")
 st.markdown(
-    '<div class="small-note">Em celulares, tente usar dois dedos (pinça) sobre a figura para dar zoom. Se a tela for estreita, também é possível deslizar horizontalmente a área do gráfico para visualizar tudo.</div>',
+    '<div class="small-note" color="#111111">Linhas contínuas representam superfícies equipotenciais. Segure o botão esquerdo do mouse e arraste para zoom em uma certa área. Clique duas vezes para restaurar a imagem.</div>',
     unsafe_allow_html=True
 )
 
